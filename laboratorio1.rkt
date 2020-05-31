49b782c3e9fc5ae380a58f8a00f07532cd5fffb1
Benjamín Donoso <benjamin.donoso@usach.cl>
2020-05-31 15:08:43 -0400

    finalizacion de las funciones add push commit pull y mayor explicacion en como opera el codigo

--- laboratorio1.rkt
+++ laboratorio1.rkt
@@ -8,31 +8,35 @@
 ;entrada un string con el comando a ejecutar
 ;salida un booleano verificando la accion
 ;funcion identificadora de comandos git
-(define git(lambda(comando info) (lambda (zonasDeTrabajo)
-    (if(procedure? comando)
-       (if (compPull comando)
-           #t
-             (if (compAdd comando)
-                  ((add info)zonasDeTrabajo)
-                  (if (compCommit comando)
-                      ((commit info)zonasDeTrabajo)
-                          (if (compPush comando)
-                          #t
-                          #f
-                      )
-                  )
-             )
-       )
-    #f
-    )
-                                 )
+(define git(lambda(comando)(lambda(info) (lambda (zonasDeTrabajo)
+                                           (if (= (largo-lista zonasDeTrabajo) 4)
+                                               (if(procedure? comando)
+                                                  (if (compPull comando)
+                                                      (pull zonasDeTrabajo)
+                                                      (if (compAdd comando)
+                                                          ((add info)zonasDeTrabajo)
+                                                          (if (compCommit comando)
+                                                              ((commit info)zonasDeTrabajo)
+                                                              (if (compPush comando)
+                                                                  (push zonasDeTrabajo)
+                          
+                                                                  (display "funcion invalida")
+                                                              )
+                                                          )
+                                                      )
+                                                  )
+                                                  (display "comando invalido")
+                                               )
+                                           (display "formato de zonas invalido")   
+                                           )
+                                        )
+                           )
             )
 )
 
 (define z1 '("xdsup" '() "dasdad" '()))
-(define z2 '("xd" '() "dasdad" '())) 
+(define z2 '("xd" "xd" "dasdad" '())) 
 
-  
     
 
 
